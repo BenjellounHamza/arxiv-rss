e0f027901c733f99805a07d1c5b6e2b4347648f5
BenjellounHamza <hmzbnjlln@gmail.com>
2021-11-03 14:50:45 +0100

    first commit

---
+++ script.py
@@ -0,0 +1,69 @@
+import os
+import sys
+import re
+from datetime import datetime
+
+domain = ['astro-ph', 'cond-mat', 'cs', 'econ', 'eess', 'gr-qc', 'hep-ex', 'hep-lat', 'hep-ph', 'hep-th', 'math', 'math-ph', 'nlin', 'nucl-ex', 'nucl-th'
+            , 'physics', 'q-bio', 'q-fin', 'quant-ph', 'stat']
+
+def find_new_articles():
+    name = ""
+    while name not in domain:
+        name = input("veillez choisir un des sujets suivants: \n " + str(domain) + " : \n\n")
+    
+    os.system("sudo ./arxiv-rss.sh " + name)
+
+    directory = "./papers"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    directory = "./papers/" + name
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    directory = "./papers/" + name + "/" + datetime.today().strftime('%Y-%m-%d')
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    file = open('/home/hamza/Bureau/arxiv-rss-master/old/2021-11-02.arxiv-others', 'r')
+    lines = file.readlines()
+    for i, line in enumerate(lines):
+        if i > 1:
+            link = get_link(line)
+            os.system("arxiv-downloader --url " + link + " --directory " + directory)
+
+
+def find_all_articles():
+
+    directory = "./papers"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    for name in domain:
+
+        os.system("sudo ./arxiv-rss.sh " + name)
+        directory = "./papers/" + name
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        directory = "./papers/" + name + "/" + datetime.today().strftime('%Y-%m-%d')
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        
+        file = open('/home/hamza/Bureau/arxiv-rss-master/old/2021-11-02.arxiv-others', 'r')
+        lines = file.readlines()
+        for i, line in enumerate(lines):
+            if i > 1:
+                link = get_link(line)
+                os.system("arxiv-downloader --url " + link + " --directory " + directory)
+
+
+
+def get_link(line):
+    return re.search("(?P<url>https?://[^\s]+)", line).group("url")
+
+if len(sys.argv) > 1:
+    for name in sys.argv[1:]:
+        find_new_articles()
+else:
+    find_all_articles()

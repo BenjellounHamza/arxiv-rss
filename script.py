e92824f4c2fbfdb2a7275eadece32d37e11bb847
BenjellounHamza <hmzbnjlln@gmail.com>
2021-11-04 16:08:32 +0100

    categories change

--- script.py
+++ script.py
@@ -4,8 +4,21 @@ import re
 from datetime import datetime
 import time
 
-domain = ['astro-ph', 'cond-mat', 'cs', 'econ', 'eess', 'gr-qc', 'hep-ex', 'hep-lat', 'hep-ph', 'hep-th', 'math', 'math-ph', 'nlin', 'nucl-ex', 'nucl-th'
-            , 'physics', 'q-bio', 'q-fin', 'quant-ph', 'stat']
+#domain = ['astro-ph', 'cond-mat', 'cs', 'econ', 'eess', 'gr-qc', 'hep-ex', 'hep-lat', 'hep-ph', 'hep-th', 'math', 'math-ph', 'nlin', 'nucl-ex', 'nucl-th'
+#            , 'physics', 'q-bio', 'q-fin', 'quant-ph', 'stat']
+
+domain = {'intelligence_artificielle' : ['cs.AI', 'cs.LG'],
+    'cybersecurity' : ['cs.DB'],
+    'robotic' : ['cs.RO'],
+    'electronic' : ['eecss'],
+    'biology' : ['q-bio'],
+    'telecomunication' : ['cs.NI'],
+    'energy' : ['astro-ph.HE', 'hep-ph', 'hep-ex', 'hep-lat'],
+    'management' : ['q-fin.PM', 'q-fin.RM'],
+    'aeronotic' : ['astro-ph'],
+    'cloud_computing': ['cs.DC']}
+
+
 
 def find_new_articles():
     name = ""
@@ -40,28 +53,26 @@ def find_all_articles():
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-    for name in domain:
+    for name, category in domain.items():
         print("waiting 15 min to extract new category")
         #time.sleep(900)
-        os.system("sudo ./arxiv-rss.sh " + name)
         directory = "./papers/" + name
         if not os.path.exists(directory):
-            os.makedirs(directory)
-
+                os.makedirs(directory)
         directory = "./papers/" + name + "/" + datetime.today().strftime('%Y-%m-%d')
         if not os.path.exists(directory):
             os.makedirs(directory)
-        else:
-            continue
-        
-        file = open('/home/hamza/Bureau/arxiv-rss-master/arxiv-others', 'r')
-        lines = file.readlines()
-        for i, line in enumerate(lines):
-            if i > 1 and i < 6:
-                link = get_link(line)
-                if link is not None:
-                    #time.sleep(10)
-                    os.system("arxiv-downloader --url " + link + " --directory " + directory)
+
+        for cat in category:
+            os.system("sudo ./arxiv-rss.sh " + cat)
+            file = open('/home/hamza/Bureau/arxiv-rss-master/arxiv-others', 'r')
+            lines = file.readlines()
+            for i, line in enumerate(lines):
+                if i > 1 and i < 6:
+                    link = get_link(line)
+                    if link is not None:
+                        #time.sleep(10)
+                        os.system("arxiv-downloader --url " + link + " --directory " + directory)
 
 
 def get_link(line):
